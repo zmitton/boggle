8ab69221c1f43e841f5e12ea8eb10bd4072782f2
zac <zacmitton22@gmail.com>
2014-08-10 15:05:38 -0500

    working version with fast binary search (database free)

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,7 +5,7 @@ end
 get '/evaluate/:board_string' do
 	if params[:board_string].length != 16 
 		params[:board_string] += "aaaaaaaaaaaaaaaa"
-		params[:board_string] = params[:board_string][0..15]
+		params[:board_string] = params[:board_string][0..15].downcase
 	end
 
 	@board_string = params[:board_string]
@@ -13,6 +13,11 @@ get '/evaluate/:board_string' do
 end
 
 get '/api/evaluate/:board_string' do
+	if params[:board_string].length != 16 
+		params[:board_string] += "aaaaaaaaaaaaaaaa"
+		params[:board_string] = params[:board_string][0..15].downcase
+	end
+
 	@board_string = params[:board_string]
 	erb :result_string, layout: false
 end
